ad24dbc92273c1c87f6fad86fe7261f2a243cf9c
Mohit Verma <learning@trayistats.com>
2025-09-09 18:49:50 +0530

    9/9/25

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Register from "./components/Register";
